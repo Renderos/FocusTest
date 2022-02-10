26db74f5eb6abb0b50f697ab20e51889c09f6cd2
Renderos <rnld.renderos24@gmail.com>
2022-02-09 23:33:01 -0600

    added scroll to bottom of the page

--- AutomationTest/src/test/java/com/focustest/junitcase/FocusSearchTest.java
+++ AutomationTest/src/test/java/com/focustest/junitcase/FocusSearchTest.java
@@ -1,18 +1,10 @@
 package com.focustest.junitcase;
-
-
-
-import static org.junit.Assert.assertEquals;
-
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.UnknownHostException;
 import java.time.Duration;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,6 +16,7 @@ public class FocusSearchTest {
 	public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
+		
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");		
 	}
@@ -34,15 +27,21 @@ public class FocusSearchTest {
 		searchbox.clear();
 		searchbox.sendKeys("Focus Services");
 		searchbox.submit();
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			WebElement searchurl = driver.findElement(By.xpath("//*[contains(text(), 'https://www.focusservices.com')]"));
 			searchurl.click();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			JavascriptExecutor jse = (JavascriptExecutor)driver;
+			jse.executeScript("window.scrollTo(0,document.body.scrollHeight);");
+			
+			
+			
 
 	}
 	
 	@After
 	public void tearDown() throws Exception {
-		
+		//driver.quit();
 	}
 
 }
